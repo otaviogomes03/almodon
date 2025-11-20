cc027ccb9a7ed3805c9453b244647200db867c89
Otavio Gomes <otaviogomes0303@gmail.com>
2025-11-20 20:02:35 -0300

    test(user): adicionar testes unitários e de integração

--- internal/domain/user/entity_test.go
+++ internal/domain/user/entity_test.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/alan-b-lima/almodon/internal/auth"
 	. "github.com/alan-b-lima/almodon/internal/domain/user"
 )
 
@@ -63,6 +64,46 @@ func TestProcessPassword(t *testing.T) {
 				t.Errorf("Password '%v': expected error, but got nil", test.input)
 			} else {
 				t.Errorf("Password '%v': did not expect error, but got: %v", test.input, err)
+			}
+		}
+	}
+}
+
+func TestNew(t *testing.T) {
+	type Tests struct {
+		siape      int
+		name       string
+		email      string
+		password   string
+		role       auth.Role
+		shouldFail bool
+	}
+
+	tests := []Tests{
+		{123456, "Alan Lima", "alan@almodon.com", "StrongPass1!", auth.Admin, false},
+		{123456, "Alan Lima", "invalid-email", "StrongPass1!", auth.User, true},
+		{123456, "Alan Lima", "alan@almodon.com", "short", auth.User, true},
+		{123456, "", "alan@almodon.com", "StrongPass1!", auth.User, true},
+		{123456, "Alan Lima", "alan@almodon.com", "StrongPass1!", auth.Role(50), true},
+	}
+
+	for _, test := range tests {
+		u, err := New(test.siape, test.name, test.email, test.password, test.role)
+
+		if (err != nil) != test.shouldFail {
+			if test.shouldFail {
+				t.Errorf("New User (Integration): expected error, but got nil. Input: %+v", test)
+			} else {
+				t.Errorf("New User (Integration): did not expect error, but got: %v. Input: %+v", err, test)
+			}
+		}
+
+		if !test.shouldFail && err == nil {
+			if u.Name() != test.name {
+				t.Errorf("New User: expected name '%s', got '%s'", test.name, u.Name())
+			}
+			if u.Role() != test.role {
+				t.Errorf("New User: expected role '%v', got '%v'", test.role, u.Role())
 			}
 		}
 	}
